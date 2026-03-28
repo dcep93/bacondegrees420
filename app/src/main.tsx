1dda4529b39ed2fa13a719d990a180316ae1cefb
Daniel Cepeda <dcep93@gmail.com>
2026-03-27 22:03:13 -0400

    x

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import AppX from "./app_x/index.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <AppX />
-  </StrictMode>,
-);
+if (window.location.pathname !== "/iframe") {
+  void import("./app_x/index.tsx").then(({ default: AppX }) => {
+    createRoot(document.getElementById("root")!).render(
+      <StrictMode>
+        <AppX />
+      </StrictMode>,
+    );
+  });
+}
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -2,5 +2,23 @@ if (
   window.location.origin === "https://www.cinenerdle2.app" &&
   window.location.pathname === "/battle"
 ) {
-  window.alert("Cinenerdle battle detected.");
+  const existingIframe = document.getElementById("bacondegrees420-iframe");
+
+  if (!existingIframe) {
+    const iframe = document.createElement("iframe");
+    iframe.id = "bacondegrees420-iframe";
+    iframe.src = "http://localhost:5173/iframe";
+    iframe.title = "BaconDegrees420";
+    iframe.style.position = "fixed";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.style.width = "100vw";
+    iframe.style.height = "100vh";
+    iframe.style.border = "0";
+    iframe.style.margin = "0";
+    iframe.style.padding = "0";
+    iframe.style.zIndex = "2147483647";
+    iframe.style.background = "#fff";
+    document.body.appendChild(iframe);
+  }
 }
