0de63ecc5c7ce2ca1a39c7c98e09ae46388e78df
Daniel Cepeda <dcep93@gmail.com>
2026-03-27 22:10:18 -0400

    x

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,29 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import AppX from "./app_x/index.tsx";
 import "./index.css";
 
-if (window.location.pathname === "/iframe") {
-  window.addEventListener("message", (event) => {
-    if (event.origin !== "https://www.cinenerdle2.app") {
-      return;
-    }
-
-    if (event.data?.type !== "bacondegrees420:rawhtml") {
-      return;
-    }
-
-    const rawHtml =
-      typeof event.data.rawHtml === "string"
-        ? event.data.rawHtml
-        : "";
-    window.alert(String(rawHtml.length));
-  });
-} else {
-  void import("./app_x/index.tsx").then(({ default: AppX }) => {
-    createRoot(document.getElementById("root")!).render(
-      <StrictMode>
-        <AppX />
-      </StrictMode>,
-    );
-  });
-}
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppX />
+  </StrictMode>,
+);
